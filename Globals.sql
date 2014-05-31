909fb2d5f2191c2e311e61fe787dcf2f579d8657
Andrea Adami <fol@fulcro.net>
2014-05-31 17:57:39 +0200

    Update Globals.sql

--- Globals.sql
+++ Globals.sql
@@ -1,9 +1,3 @@
---
--- PostgreSQL database cluster dump
---
-
--- Started on 2014-05-30 19:19:49
-
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 
@@ -12,9 +6,7 @@ SET standard_conforming_strings = on;
 --
 
 CREATE ROLE "fol@scuola247.org";
-ALTER ROLE "fol@scuola247.org" WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION VALID UNTIL 'infinity';
-CREATE ROLE postgres;
-ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION PASSWORD 'password' VALID UNTIL 'infinity';
+ALTER ROLE "fol@scuola247.org" WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION PASSWORD 'password' VALID UNTIL 'infinity';
 CREATE ROLE scuola247;
 ALTER ROLE scuola247 WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION VALID UNTIL 'infinity';
 CREATE ROLE scuola247_manager;
@@ -26,13 +18,5 @@ ALTER ROLE scuola247_manager WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NO
 --
 
 GRANT scuola247_manager TO "fol@scuola247.org" GRANTED BY postgres;
-
-
-
-
--- Completed on 2014-05-30 19:19:49
-
---
--- PostgreSQL database cluster dump complete
---
+GRANT scuola247 TO "fol@scuola247.org" GRANTED BY postgres;
 
