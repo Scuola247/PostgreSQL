7804c2e6c990f5c2a32b7b886e17890d17887be5
Andrea Adami <fol@fulcro.net>
2017-10-02 19:37:32 +0200

    Roles need to Scuola247
    Please modify the password before to run the scrit

--- globals.sql
+++ globals.sql
@@ -1,12 +1,33 @@
---
--- PostgreSQL database cluster dump
---
---
--- l'utente postgres è stato tolto
--- nessun utente ha la password
---
+/* 
+Copyright (C) 2014-2017 FULCRO SRL (http://www.fulcro.net)
+  
+This file is part of Scuola247 project (http://www.scuola247.org).
 
--- Started on 2016-12-14 06:52:45
+Scuola247 is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License version 3
+as published by the Free Software Foundation.
+
+Scuola247 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+PLEASE 
+PLEASE
+PLEASE
+
+Note that the password: 'md5 xxxxxx' is NOT a valid password.
+please set a newone before to run the script
+
+PLEASE 
+PLEASE
+PLEASE
+*/
 
 SET default_transaction_read_only = off;
 
@@ -17,35 +38,115 @@ SET standard_conforming_strings = on;
 -- Roles
 --
 
-CREATE ROLE "fol@scuola247.it";
-ALTER ROLE "fol@scuola247.it" WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION NOBYPASSRLS VALID UNTIL 'infinity';
-CREATE ROLE "manager@scuola247.it";
-ALTER ROLE "manager@scuola247.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS VALID UNTIL 'infinity';
-CREATE ROLE scuola247_manager;
-ALTER ROLE scuola247_manager WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS VALID UNTIL 'infinity';
-COMMENT ON ROLE scuola247_manager IS 'Tutte le autorizzazioni tolte al ruolo public per il database scuola247 sono date a questo ruolo che contiene gli utenti gestori';
+CREATE ROLE "manager-a@scuola-1.it";
+ALTER ROLE "manager-a@scuola-1.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "manager-b@scuola-1.it";
+ALTER ROLE "manager-b@scuola-1.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "manager-c@scuola-2.it";
+ALTER ROLE "manager-c@scuola-2.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "manager-d@scuola-2.it";
+ALTER ROLE "manager-d@scuola-2.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "manager-e@scuola-28961.it";
+ALTER ROLE "manager-e@scuola-28961.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "manager-f@scuola-28961.it";
+ALTER ROLE "manager-f@scuola-28961.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "parent-a@scuola-1.it";
+ALTER ROLE "parent-a@scuola-1.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "parent-b@scuola-1.it";
+ALTER ROLE "parent-b@scuola-1.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "parent-c@scuola-2.it";
+ALTER ROLE "parent-c@scuola-2.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "parent-d@scuola-2.it";
+ALTER ROLE "parent-d@scuola-2.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "parent-e@scuola-28961.it";
+ALTER ROLE "parent-e@scuola-28961.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "parent-f@scuola-28961.it";
+ALTER ROLE "parent-f@scuola-28961.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE postgres;
+ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE postgrest;
+ALTER ROLE postgrest WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
+COMMENT ON ROLE postgrest IS 'Role usato dal tools PostGREST (un Rest Server) per l''impersonificazione delle richieste anonime';
+CREATE ROLE scuola247_employee;
+ALTER ROLE scuola247_employee WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
+COMMENT ON ROLE scuola247_employee IS 'Group for the school''s employees';
+CREATE ROLE scuola247_executive;
+ALTER ROLE scuola247_executive WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
+COMMENT ON ROLE scuola247_executive IS 'Group for the school''s executive';
+CREATE ROLE scuola247_relative;
+ALTER ROLE scuola247_relative WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
+COMMENT ON ROLE scuola247_relative IS 'Group for the school''s relative';
+CREATE ROLE scuola247_student;
+ALTER ROLE scuola247_student WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
+COMMENT ON ROLE scuola247_student IS 'Group for the school''s student';
+CREATE ROLE scuola247_supervisor;
+ALTER ROLE scuola247_supervisor WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
+COMMENT ON ROLE scuola247_supervisor IS 'Group for the system''s administratiom';
+CREATE ROLE scuola247_teacher;
+ALTER ROLE scuola247_teacher WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
+COMMENT ON ROLE scuola247_teacher IS 'Group for the school''s teachers';
 CREATE ROLE scuola247_user;
-ALTER ROLE scuola247_user WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS VALID UNTIL 'infinity';
-COMMENT ON ROLE scuola247_user IS 'Sul database scuola247 è tolto al ruolo predefinito PUBLIC qualsiasi autorizzazione.
-Questo ruolo abilita al login e al richiamo delle funzioni a cui è delegato il controllo della sicurezza.
-Concettualmente quindi il singolo utente non accede mai direttamente alle tabelle o alle viste ma solo tramite funzioni.
-';
-CREATE ROLE "teacher@scuola247.it";
-ALTER ROLE "teacher@scuola247.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md547a6b361760f88a0422c2789806a6151' VALID UNTIL 'infinity';
+ALTER ROLE scuola247_user WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
+COMMENT ON ROLE scuola247_user IS 'Group for the school''s users';
+CREATE ROLE "student-a@scuola-1.it";
+ALTER ROLE "student-a@scuola-1.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "student-b@scuola-1.it";
+ALTER ROLE "student-b@scuola-1.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "student-c@scuola-2.it";
+ALTER ROLE "student-c@scuola-2.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "student-d@scuola-2.it";
+ALTER ROLE "student-d@scuola-2.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "student-e@scuola-28961.it";
+ALTER ROLE "student-e@scuola-28961.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "student-f@scuola-28961.it";
+ALTER ROLE "student-f@scuola-28961.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "teacher-a@scuola-1.it";
+ALTER ROLE "teacher-a@scuola-1.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "teacher-b@scuola-1.it";
+ALTER ROLE "teacher-b@scuola-1.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "teacher-c@scuola-2.it";
+ALTER ROLE "teacher-c@scuola-2.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "teacher-d@scuola-2.it";
+ALTER ROLE "teacher-d@scuola-2.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "teacher-e@scuola-28961.it";
+ALTER ROLE "teacher-e@scuola-28961.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
+CREATE ROLE "teacher-f@scuola-28961.it";
+ALTER ROLE "teacher-f@scuola-28961.it" WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md5 xxxxxx';
 
 --
 -- Role memberships
 --
 
-GRANT scuola247_manager TO "fol@scuola247.it" GRANTED BY postgres;
-GRANT scuola247_manager TO "manager@scuola247.it" GRANTED BY postgres;
-GRANT scuola247_user TO "fol@scuola247.it" GRANTED BY postgres;
-GRANT scuola247_user TO "manager@scuola247.it" GRANTED BY postgres;
-GRANT scuola247_user TO "teacher@scuola247.it" GRANTED BY postgres;
-
-
--- Completed on 2016-12-14 06:52:45
-
---
--- PostgreSQL database cluster dump complete
---
+GRANT postgrest TO postgres GRANTED BY postgres;
+GRANT scuola247_employee TO postgres GRANTED BY postgres;
+GRANT scuola247_executive TO "manager-a@scuola-1.it" GRANTED BY postgres;
+GRANT scuola247_executive TO "manager-b@scuola-1.it" GRANTED BY postgres;
+GRANT scuola247_executive TO "manager-c@scuola-2.it" GRANTED BY postgres;
+GRANT scuola247_executive TO "manager-d@scuola-2.it" GRANTED BY postgres;
+GRANT scuola247_executive TO "manager-e@scuola-28961.it" GRANTED BY postgres;
+GRANT scuola247_executive TO "manager-f@scuola-28961.it" GRANTED BY postgres;
+GRANT scuola247_executive TO postgres GRANTED BY postgres;
+GRANT scuola247_relative TO "parent-a@scuola-1.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "parent-b@scuola-1.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "parent-c@scuola-2.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "parent-d@scuola-2.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "parent-e@scuola-28961.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "parent-f@scuola-28961.it" GRANTED BY postgres;
+GRANT scuola247_relative TO postgres GRANTED BY postgres;
+GRANT scuola247_relative TO "student-a@scuola-1.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "student-b@scuola-1.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "student-c@scuola-2.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "student-d@scuola-2.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "student-e@scuola-28961.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "student-f@scuola-28961.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "teacher-a@scuola-1.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "teacher-b@scuola-1.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "teacher-c@scuola-2.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "teacher-d@scuola-2.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "teacher-e@scuola-28961.it" GRANTED BY postgres;
+GRANT scuola247_relative TO "teacher-f@scuola-28961.it" GRANTED BY postgres;
+GRANT scuola247_student TO postgres GRANTED BY postgres;
+GRANT scuola247_supervisor TO postgres GRANTED BY postgres;
+GRANT scuola247_teacher TO postgres GRANTED BY postgres;
+GRANT scuola247_user TO postgres GRANTED BY postgres;
+GRANT scuola247_user TO scuola247_executive GRANTED BY postgres;
