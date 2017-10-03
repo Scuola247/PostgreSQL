de7049ce41b9b93bcd4cf7c972e153de673da8be
Andrea Adami <fol@fulcro.net>
2017-10-03 19:42:08 +0200

    utility schema and data
    Script to create the utility schema's objects and populate them with data

---
+++ utility/schema_data.sql
@@ -0,0 +1,17 @@
+/* 
+Copyright (C) 2014-2017 FULCRO SRL (http://www.fulcro.net)
+  
+This file is part of Scuola247 project (http://www.scuola247.org).
+
+Scuola247 is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License version 3
+as published by the Free Software Foundation.
+
+Scuola247 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
+*/
