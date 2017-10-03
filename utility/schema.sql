544016596cb5f8918969fb8ad845517b2f06a48e
Andrea Adami <fol@fulcro.net>
2017-10-03 20:19:58 +0200

    Update schema.sql

--- utility/schema.sql
+++ utility/schema.sql
@@ -36,7 +36,7 @@ CREATE SCHEMA utility;
 ALTER SCHEMA utility OWNER TO postgres;
 
 --
--- TOC entry 3723 (class 0 OID 0)
+-- TOC entry 3704 (class 0 OID 0)
 -- Dependencies: 17
 -- Name: SCHEMA utility; Type: COMMENT; Schema: -; Owner: postgres
 --
@@ -71,7 +71,7 @@ CREATE TYPE language AS ENUM (
 ALTER TYPE language OWNER TO postgres;
 
 --
--- TOC entry 3724 (class 0 OID 0)
+-- TOC entry 3705 (class 0 OID 0)
 -- Dependencies: 1333
 -- Name: TYPE language; Type: COMMENT; Schema: utility; Owner: postgres
 --
@@ -92,7 +92,7 @@ COMMENT ON TYPE language IS '''de'' Deutsch
 
 
 --
--- TOC entry 1715 (class 1247 OID 759688)
+-- TOC entry 1697 (class 1247 OID 759688)
 -- Name: number_base34; Type: DOMAIN; Schema: utility; Owner: postgres
 --
 
@@ -103,8 +103,8 @@ CREATE DOMAIN number_base34 AS character varying(12)
 ALTER DOMAIN number_base34 OWNER TO postgres;
 
 --
--- TOC entry 3725 (class 0 OID 0)
--- Dependencies: 1715
+-- TOC entry 3706 (class 0 OID 0)
+-- Dependencies: 1697
 -- Name: DOMAIN number_base34; Type: COMMENT; Schema: utility; Owner: postgres
 --
 
@@ -140,7 +140,7 @@ CREATE DOMAIN week_day AS smallint
 ALTER DOMAIN week_day OWNER TO postgres;
 
 --
--- TOC entry 3726 (class 0 OID 0)
+-- TOC entry 3707 (class 0 OID 0)
 -- Dependencies: 1339
 -- Name: DOMAIN week_day; Type: COMMENT; Schema: utility; Owner: postgres
 --
@@ -173,7 +173,7 @@ $$;
 ALTER FUNCTION utility.day_name(_weekday week_day) OWNER TO postgres;
 
 --
--- TOC entry 3727 (class 0 OID 0)
+-- TOC entry 3708 (class 0 OID 0)
 -- Dependencies: 499
 -- Name: FUNCTION day_name(_weekday week_day); Type: COMMENT; Schema: utility; Owner: postgres
 --
@@ -225,7 +225,7 @@ $$;
 ALTER FUNCTION utility.count_value(search_db text, search_schema text, search_table text) OWNER TO postgres;
 
 --
--- TOC entry 3728 (class 0 OID 0)
+-- TOC entry 3709 (class 0 OID 0)
 -- Dependencies: 636
 -- Name: FUNCTION count_value(search_db text, search_schema text, search_table text); Type: COMMENT; Schema: utility; Owner: postgres
 --
@@ -235,7 +235,7 @@ Restituisce l''elenco delle colonne che ammettono valori null person l''indicazi
 
 
 --
--- TOC entry 538 (class 1255 OID 19379)
+-- TOC entry 542 (class 1255 OID 19379)
 -- Name: entity2char(text); Type: FUNCTION; Schema: utility; Owner: postgres
 --
 
@@ -248,7 +248,7 @@ begin
     for r in
         select distinct ce.coding as ch, ce.entity as name
         from
-            utility.entity_coding() ce
+            datasets.entity_coding() ce
             inner join (
                 select name[1] "name"
                 from regexp_matches(t, '&([A-Za-z]+?);', 'g') r(name)
@@ -283,8 +283,8 @@ $$;
 ALTER FUNCTION utility.entity2char(t text) OWNER TO postgres;
 
 --
--- TOC entry 3730 (class 0 OID 0)
--- Dependencies: 538
+-- TOC entry 3711 (class 0 OID 0)
+-- Dependencies: 542
 -- Name: FUNCTION entity2char(t text); Type: COMMENT; Schema: utility; Owner: postgres
 --
 
@@ -292,7 +292,7 @@ COMMENT ON FUNCTION entity2char(t text) IS '<utility>';
 
 
 --
--- TOC entry 539 (class 1255 OID 19380)
+-- TOC entry 538 (class 1255 OID 19380)
 -- Name: entity_coding(); Type: FUNCTION; Schema: utility; Owner: postgres
 --
 
@@ -561,8 +561,8 @@ $$;
 ALTER FUNCTION utility.entity_coding() OWNER TO postgres;
 
 --
--- TOC entry 3731 (class 0 OID 0)
--- Dependencies: 539
+-- TOC entry 3712 (class 0 OID 0)
+-- Dependencies: 538
 -- Name: FUNCTION entity_coding(); Type: COMMENT; Schema: utility; Owner: postgres
 --
 
@@ -586,7 +586,7 @@ $$;
 ALTER FUNCTION utility.enum2array(_enum anyenum) OWNER TO postgres;
 
 --
--- TOC entry 3732 (class 0 OID 0)
+-- TOC entry 3713 (class 0 OID 0)
 -- Dependencies: 900
 -- Name: FUNCTION enum2array(_enum anyenum); Type: COMMENT; Schema: utility; Owner: postgres
 --
@@ -625,7 +625,7 @@ $$;
 ALTER FUNCTION utility.get_max_column_value(schema_name text, table_name text, column_name text, OUT max_value bigint) OWNER TO postgres;
 
 --
--- TOC entry 3733 (class 0 OID 0)
+-- TOC entry 3714 (class 0 OID 0)
 -- Dependencies: 537
 -- Name: FUNCTION get_max_column_value(schema_name text, table_name text, column_name text, OUT max_value bigint); Type: COMMENT; Schema: utility; Owner: postgres
 --
@@ -774,7 +774,7 @@ $$;
 ALTER FUNCTION utility.set_all_sequences_2_the_max() OWNER TO postgres;
 
 --
--- TOC entry 3734 (class 0 OID 0)
+-- TOC entry 3715 (class 0 OID 0)
 -- Dependencies: 543
 -- Name: FUNCTION set_all_sequences_2_the_max(); Type: COMMENT; Schema: utility; Owner: postgres
 --
@@ -817,7 +817,7 @@ $$;
 ALTER FUNCTION utility.set_sequence_2_the_max(_sequence_schema_name text, _sequence_name text) OWNER TO postgres;
 
 --
--- TOC entry 3735 (class 0 OID 0)
+-- TOC entry 3716 (class 0 OID 0)
 -- Dependencies: 929
 -- Name: FUNCTION set_sequence_2_the_max(_sequence_schema_name text, _sequence_name text); Type: COMMENT; Schema: utility; Owner: postgres
 --
@@ -841,7 +841,7 @@ $_$;
 ALTER FUNCTION utility.strip_tags(text) OWNER TO postgres;
 
 --
--- TOC entry 3736 (class 0 OID 0)
+-- TOC entry 3717 (class 0 OID 0)
 -- Dependencies: 544
 -- Name: FUNCTION strip_tags(text); Type: COMMENT; Schema: utility; Owner: postgres
 --
@@ -1011,7 +1011,7 @@ $$;
 ALTER FUNCTION utility.url_decode(encode_url text) OWNER TO postgres;
 
 --
--- TOC entry 3737 (class 0 OID 0)
+-- TOC entry 3718 (class 0 OID 0)
 -- Dependencies: 545
 -- Name: FUNCTION url_decode(encode_url text); Type: COMMENT; Schema: utility; Owner: postgres
 --
@@ -1085,7 +1085,7 @@ $$;
 ALTER FUNCTION utility.where_sequence(p_catalog text, p_schema text, p_sequence text, p_value bigint) OWNER TO postgres;
 
 --
--- TOC entry 3738 (class 0 OID 0)
+-- TOC entry 3719 (class 0 OID 0)
 -- Dependencies: 546
 -- Name: FUNCTION where_sequence(p_catalog text, p_schema text, p_sequence text, p_value bigint); Type: COMMENT; Schema: utility; Owner: postgres
 --
@@ -1097,7 +1097,7 @@ So it can be usefull, starting from a value of the sequence, go back to the tabl
 
 
 --
--- TOC entry 2662 (class 2617 OID 759710)
+-- TOC entry 2654 (class 2617 OID 759710)
 -- Name: +; Type: OPERATOR; Schema: utility; Owner: postgres
 --
 
@@ -1111,113 +1111,8 @@ CREATE OPERATOR + (
 
 ALTER OPERATOR utility.+ (number_base34, integer) OWNER TO postgres;
 
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
 --
--- TOC entry 331 (class 1259 OID 19930)
--- Name: ip_addresses; Type: TABLE; Schema: utility; Owner: postgres
---
-
-CREATE TABLE ip_addresses (
-    ip_address bigint DEFAULT nextval('public.pk_seq'::regclass) NOT NULL,
-    from_ip inet NOT NULL,
-    to_ip inet NOT NULL,
-    country character varying(2) NOT NULL
-);
-
-
-ALTER TABLE ip_addresses OWNER TO postgres;
-
---
--- TOC entry 3739 (class 0 OID 0)
--- Dependencies: 331
--- Name: TABLE ip_addresses; Type: COMMENT; Schema: utility; Owner: postgres
---
-
-COMMENT ON TABLE ip_addresses IS 'ip addresses allocate by ripe get from: 
-https://db-ip.com/';
-
-
---
--- TOC entry 332 (class 1259 OID 19937)
--- Name: apache2_conf; Type: VIEW; Schema: utility; Owner: postgres
---
-
-CREATE VIEW apache2_conf AS
- SELECT ('Require ip '::text || inet_merge(ip_addresses.from_ip, ip_addresses.to_ip)) AS command
-   FROM ip_addresses
-  WHERE ((ip_addresses.country)::text = ANY (ARRAY[('IT'::character varying)::text, ('FR'::character varying)::text, ('DE'::character varying)::text]))
-  ORDER BY ip_addresses.country, ip_addresses.from_ip;
-
-
-ALTER TABLE apache2_conf OWNER TO postgres;
-
---
--- TOC entry 3740 (class 0 OID 0)
--- Dependencies: 332
--- Name: VIEW apache2_conf; Type: COMMENT; Schema: utility; Owner: postgres
---
-
-COMMENT ON VIEW apache2_conf IS 'list of statement to put in apache2_conf to allow connection from listed addresses ip';
-
-
---
--- TOC entry 333 (class 1259 OID 19941)
--- Name: entity_coding2; Type: TABLE; Schema: utility; Owner: postgres
---
-
-CREATE TABLE entity_coding2 (
-    entity text NOT NULL,
-    coding character(1)
-);
-
-
-ALTER TABLE entity_coding2 OWNER TO postgres;
-
---
--- TOC entry 3741 (class 0 OID 0)
--- Dependencies: 333
--- Name: TABLE entity_coding2; Type: COMMENT; Schema: utility; Owner: postgres
---
-
-COMMENT ON TABLE entity_coding2 IS 'create table character_entity(
-    name text primary key,
-    ch char(1) unique
-);
-insert into character_entity (ch, name) values
-    (E''\u00C6'',''AElig''),(E''\u00C1'',''Aacute''),(E''\u00C2'',''Acirc''),(E''\u00C0'',''Agrave''),(E''\u0391'',''Alpha''),(E''\u00C5'',''Aring''),(E''\u00C3'',''Atilde''),(E''\u00C4'',''Auml''),(E''\u0392'',''Beta''),(E''\u00C7'',''Ccedil''),
-    (E''\u03A7'',''Chi''),(E''\u2021'',''Dagger''),(E''\u0394'',''Delta''),(E''\u00D0'',''ETH''),(E''\u00C9'',''Eacute''),(E''\u00CA'',''Ecirc''),(E''\u00C8'',''Egrave''),(E''\u0395'',''Epsilon''),(E''\u0397'',''Eta''),(E''\u00CB'',''Euml''),
-    (E''\u0393'',''Gamma''),(E''\u00CD'',''Iacute''),(E''\u00CE'',''Icirc''),(E''\u00CC'',''Igrave''),(E''\u0399'',''Iota''),(E''\u00CF'',''Iuml''),(E''\u039A'',''Kappa''),(E''\u039B'',''Lambda''),(E''\u039C'',''Mu''),(E''\u00D1'',''Ntilde''),
-    (E''\u039D'',''Nu''),(E''\u0152'',''OElig''),(E''\u00D3'',''Oacute''),(E''\u00D4'',''Ocirc''),(E''\u00D2'',''Ograve''),(E''\u03A9'',''Omega''),(E''\u039F'',''Omicron''),(E''\u00D8'',''Oslash''),(E''\u00D5'',''Otilde''),(E''\u00D6'',''Ouml''),
-    (E''\u03A6'',''Phi''),(E''\u03A0'',''Pi''),(E''\u2033'',''Prime''),(E''\u03A8'',''Psi''),(E''\u03A1'',''Rho''),(E''\u0160'',''Scaron''),(E''\u03A3'',''Sigma''),(E''\u00DE'',''THORN''),(E''\u03A4'',''Tau''),(E''\u0398'',''Theta''),
-    (E''\u00DA'',''Uacute''),(E''\u00DB'',''Ucirc''),(E''\u00D9'',''Ugrave''),(E''\u03A5'',''Upsilon''),(E''\u00DC'',''Uuml''),(E''\u039E'',''Xi''),(E''\u00DD'',''Yacute''),(E''\u0178'',''Yuml''),(E''\u0396'',''Zeta''),(E''\u00E1'',''aacute''),
-    (E''\u00E2'',''acirc''),(E''\u00B4'',''acute''),(E''\u00E6'',''aelig''),(E''\u00E0'',''agrave''),(E''\u2135'',''alefsym''),(E''\u03B1'',''alpha''),(E''\u0026'',''amp''),(E''\u2227'',''and''),(E''\u2220'',''ang''),(E''\u00E5'',''aring''),
-    (E''\u2248'',''asymp''),(E''\u00E3'',''atilde''),(E''\u00E4'',''auml''),(E''\u201E'',''bdquo''),(E''\u03B2'',''beta''),(E''\u00A6'',''brvbar''),(E''\u2022'',''bull''),(E''\u2229'',''cap''),(E''\u00E7'',''ccedil''),(E''\u00B8'',''cedil''),
-    (E''\u00A2'',''cent''),(E''\u03C7'',''chi''),(E''\u02C6'',''circ''),(E''\u2663'',''clubs''),(E''\u2245'',''cong''),(E''\u00A9'',''copy''),(E''\u21B5'',''crarr''),(E''\u222A'',''cup''),(E''\u00A4'',''curren''),(E''\u21D3'',''dArr''),
-    (E''\u2020'',''dagger''),(E''\u2193'',''darr''),(E''\u00B0'',''deg''),(E''\u03B4'',''delta''),(E''\u2666'',''diams''),(E''\u00F7'',''divide''),(E''\u00E9'',''eacute''),(E''\u00EA'',''ecirc''),(E''\u00E8'',''egrave''),(E''\u2205'',''empty''),
-    (E''\u2003'',''emsp''),(E''\u2002'',''ensp''),(E''\u03B5'',''epsilon''),(E''\u2261'',''equiv''),(E''\u03B7'',''eta''),(E''\u00F0'',''eth''),(E''\u00EB'',''euml''),(E''\u20AC'',''euro''),(E''\u2203'',''exist''),(E''\u0192'',''fnof''),
-    (E''\u2200'',''forall''),(E''\u00BD'',''frac12''),(E''\u00BC'',''frac14''),(E''\u00BE'',''frac34''),(E''\u2044'',''frasl''),(E''\u03B3'',''gamma''),(E''\u2265'',''ge''),(E''\u003E'',''gt''),(E''\u21D4'',''hArr''),(E''\u2194'',''harr''),
-    (E''\u2665'',''hearts''),(E''\u2026'',''hellip''),(E''\u00ED'',''iacute''),(E''\u00EE'',''icirc''),(E''\u00A1'',''iexcl''),(E''\u00EC'',''igrave''),(E''\u2111'',''image''),(E''\u221E'',''infin''),(E''\u222B'',''int''),(E''\u03B9'',''iota''),
-    (E''\u00BF'',''iquest''),(E''\u2208'',''isin''),(E''\u00EF'',''iuml''),(E''\u03BA'',''kappa''),(E''\u21D0'',''lArr''),(E''\u03BB'',''lambda''),(E''\u2329'',''lang''),(E''\u00AB'',''laquo''),(E''\u2190'',''larr''),(E''\u2308'',''lceil''),
-    (E''\u201C'',''ldquo''),(E''\u2264'',''le''),(E''\u230A'',''lfloor''),(E''\u2217'',''lowast''),(E''\u25CA'',''loz''),(E''\u200E'',''lrm''),(E''\u2039'',''lsaquo''),(E''\u2018'',''lsquo''),(E''\u003C'',''lt''),(E''\u00AF'',''macr''),
-    (E''\u2014'',''mdash''),(E''\u00B5'',''micro''),(E''\u00B7'',''middot''),(E''\u2212'',''minus''),(E''\u03BC'',''mu''),(E''\u2207'',''nabla''),(E''\u00A0'',''nbsp''),(E''\u2013'',''ndash''),(E''\u2260'',''ne''),(E''\u220B'',''ni''),
-    (E''\u00AC'',''not''),(E''\u2209'',''notin''),(E''\u2284'',''nsub''),(E''\u00F1'',''ntilde''),(E''\u03BD'',''nu''),(E''\u00F3'',''oacute''),(E''\u00F4'',''ocirc''),(E''\u0153'',''oelig''),(E''\u00F2'',''ograve''),(E''\u203E'',''oline''),
-    (E''\u03C9'',''omega''),(E''\u03BF'',''omicron''),(E''\u2295'',''oplus''),(E''\u2228'',''or''),(E''\u00AA'',''ordf''),(E''\u00BA'',''ordm''),(E''\u00F8'',''oslash''),(E''\u00F5'',''otilde''),(E''\u2297'',''otimes''),(E''\u00F6'',''ouml''),
-    (E''\u00B6'',''para''),(E''\u2202'',''part''),(E''\u2030'',''permil''),(E''\u22A5'',''perp''),(E''\u03C6'',''phi''),(E''\u03C0'',''pi''),(E''\u03D6'',''piv''),(E''\u00B1'',''plusmn''),(E''\u00A3'',''pound''),(E''\u2032'',''prime''),
-    (E''\u220F'',''prod''),(E''\u221D'',''prop''),(E''\u03C8'',''psi''),(E''\u0022'',''quot''),(E''\u21D2'',''rArr''),(E''\u221A'',''radic''),(E''\u232A'',''rang''),(E''\u00BB'',''raquo''),(E''\u2192'',''rarr''),(E''\u2309'',''rceil''),
-    (E''\u201D'',''rdquo''),(E''\u211C'',''real''),(E''\u00AE'',''reg''),(E''\u230B'',''rfloor''),(E''\u03C1'',''rho''),(E''\u200F'',''rlm''),(E''\u203A'',''rsaquo''),(E''\u2019'',''rsquo''),(E''\u201A'',''sbquo''),(E''\u0161'',''scaron''),
-    (E''\u22C5'',''sdot''),(E''\u00A7'',''sect''),(E''\u00AD'',''shy''),(E''\u03C3'',''sigma''),(E''\u03C2'',''sigmaf''),(E''\u223C'',''sim''),(E''\u2660'',''spades''),(E''\u2282'',''sub''),(E''\u2286'',''sube''),(E''\u2211'',''sum''),
-    (E''\u2283'',''sup''),(E''\u00B9'',''sup1''),(E''\u00B2'',''sup2''),(E''\u00B3'',''sup3''),(E''\u2287'',''supe''),(E''\u00DF'',''szlig''),(E''\u03C4'',''tau''),(E''\u2234'',''there4''),(E''\u03B8'',''theta''),(E''\u03D1'',''thetasym''),
-    (E''\u2009'',''thinsp''),(E''\u00FE'',''thorn''),(E''\u02DC'',''tilde''),(E''\u00D7'',''times''),(E''\u2122'',''trade''),(E''\u21D1'',''uArr''),(E''\u00FA'',''uacute''),(E''\u2191'',''uarr''),(E''\u00FB'',''ucirc''),(E''\u00F9'',''ugrave''),
-    (E''\u00A8'',''uml''),(E''\u03D2'',''upsih''),(E''\u03C5'',''upsilon''),(E''\u00FC'',''uuml''),(E''\u2118'',''weierp''),(E''\u03BE'',''xi''),(E''\u00FD'',''yacute''),(E''\u00A5'',''yen''),(E''\u00FF'',''yuml''),(E''\u03B6'',''zeta''),
-    (E''\u200D'',''zwj''),(E''\u200C'',''zwnj'')
-;';
-
-
---
--- TOC entry 334 (class 1259 OID 19948)
+-- TOC entry 332 (class 1259 OID 19948)
 -- Name: enums_values; Type: VIEW; Schema: utility; Owner: postgres
 --
 
@@ -1233,8 +1128,8 @@ CREATE VIEW enums_values AS
 ALTER TABLE enums_values OWNER TO postgres;
 
 --
--- TOC entry 3742 (class 0 OID 0)
--- Dependencies: 334
+-- TOC entry 3720 (class 0 OID 0)
+-- Dependencies: 332
 -- Name: VIEW enums_values; Type: COMMENT; Schema: utility; Owner: postgres
 --
 
@@ -1242,20 +1137,7 @@ COMMENT ON VIEW enums_values IS 'List all enum and all related values';
 
 
 --
--- TOC entry 335 (class 1259 OID 19953)
--- Name: ip_addresses_country_list; Type: VIEW; Schema: utility; Owner: postgres
---
-
-CREATE VIEW ip_addresses_country_list AS
- SELECT DISTINCT ip_addresses.country
-   FROM ip_addresses
-  ORDER BY ip_addresses.country;
-
-
-ALTER TABLE ip_addresses_country_list OWNER TO postgres;
-
---
--- TOC entry 336 (class 1259 OID 19957)
+-- TOC entry 333 (class 1259 OID 19957)
 -- Name: pk_seq; Type: SEQUENCE; Schema: utility; Owner: postgres
 --
 
@@ -1270,7 +1152,7 @@ CREATE SEQUENCE pk_seq
 ALTER TABLE pk_seq OWNER TO postgres;
 
 --
--- TOC entry 337 (class 1259 OID 19959)
+-- TOC entry 334 (class 1259 OID 19959)
 -- Name: sequence_references; Type: VIEW; Schema: utility; Owner: postgres
 --
 
@@ -1294,8 +1176,8 @@ CREATE VIEW sequence_references AS
 ALTER TABLE sequence_references OWNER TO postgres;
 
 --
--- TOC entry 3743 (class 0 OID 0)
--- Dependencies: 337
+-- TOC entry 3721 (class 0 OID 0)
+-- Dependencies: 334
 -- Name: VIEW sequence_references; Type: COMMENT; Schema: utility; Owner: postgres
 --
 
@@ -1303,86 +1185,11 @@ COMMENT ON VIEW sequence_references IS 'List tables and columns where sequences 
 
 
 --
--- TOC entry 338 (class 1259 OID 19964)
--- Name: usenames_ex; Type: TABLE; Schema: utility; Owner: postgres
---
-
-CREATE TABLE usenames_ex (
-    usename name NOT NULL,
-    token character varying(1024),
-    language language DEFAULT 'it'::language NOT NULL
-);
-
-
-ALTER TABLE usenames_ex OWNER TO postgres;
-
---
--- TOC entry 3744 (class 0 OID 0)
--- Dependencies: 338
--- Name: TABLE usenames_ex; Type: COMMENT; Schema: utility; Owner: postgres
---
-
-COMMENT ON TABLE usenames_ex IS 'add informations to usename''s system table';
-
-
---
--- TOC entry 3745 (class 0 OID 0)
--- Dependencies: 338
--- Name: COLUMN usenames_ex.token; Type: COMMENT; Schema: utility; Owner: postgres
---
-
-COMMENT ON COLUMN usenames_ex.token IS 'serve per il ripristino della password street email';
-
-
---
--- TOC entry 3432 (class 2606 OID 138707)
--- Name: entity_coding2 character_entity_ch_key; Type: CONSTRAINT; Schema: utility; Owner: postgres
---
-
-ALTER TABLE ONLY entity_coding2
-    ADD CONSTRAINT character_entity_ch_key UNIQUE (coding);
-
-
---
--- TOC entry 3434 (class 2606 OID 138709)
--- Name: entity_coding2 character_entity_pkey; Type: CONSTRAINT; Schema: utility; Owner: postgres
---
-
-ALTER TABLE ONLY entity_coding2
-    ADD CONSTRAINT character_entity_pkey PRIMARY KEY (entity);
-
-
---
--- TOC entry 3430 (class 2606 OID 138711)
--- Name: ip_addresses ip_addresses_pkey; Type: CONSTRAINT; Schema: utility; Owner: postgres
---
-
-ALTER TABLE ONLY ip_addresses
-    ADD CONSTRAINT ip_addresses_pkey PRIMARY KEY (ip_address);
-
-
---
--- TOC entry 3436 (class 2606 OID 138713)
--- Name: usenames_ex usenames_ex_pk; Type: CONSTRAINT; Schema: utility; Owner: postgres
---
-
-ALTER TABLE ONLY usenames_ex
-    ADD CONSTRAINT usenames_ex_pk PRIMARY KEY (usename);
-
-
---
--- TOC entry 3437 (class 2620 OID 138855)
--- Name: usenames_ex usenames_ex_iu; Type: TRIGGER; Schema: utility; Owner: postgres
---
-
-CREATE TRIGGER usenames_ex_iu AFTER INSERT OR UPDATE ON usenames_ex FOR EACH ROW EXECUTE PROCEDURE tr_usenames_ex_iu();
-
-
---
--- TOC entry 3729 (class 0 OID 0)
+-- TOC entry 3710 (class 0 OID 0)
 -- Dependencies: 636
 -- Name: count_value(text, text, text); Type: ACL; Schema: utility; Owner: postgres
 --
 
 REVOKE ALL ON FUNCTION count_value(search_db text, search_schema text, search_table text) FROM PUBLIC;
 GRANT ALL ON FUNCTION count_value(search_db text, search_schema text, search_table text) TO scuola247_relative;
+
