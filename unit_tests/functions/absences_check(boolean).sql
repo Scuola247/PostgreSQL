6dfe40abd885b640174a5b5edca3b457dc085f55
TzoME <>
2018-05-30 10:45:15 +0200

    fix exception in units_tests.absences_check(boolean)

--- unit_tests/functions/absences_check(boolean).sql
+++ unit_tests/functions/absences_check(boolean).sql
@@ -40,18 +40,13 @@ BEGIN
    _results =  _results || assert.fail(full_function_name, test_name, 'Insert was OK but on_date NULL value was expected', NULL::diagnostic.error);      
     RETURN;      
 
-    EXCEPTION WHEN SQLSTATE '23505' THEN 
-      GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;	
-      IF error.constraint_name = 'schools_uq_description' THEN
-        _results =  _results || assert.pass(full_function_name, test_name);
-      ELSE
-	_results =  _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);      
-	RETURN; 
-      END IF; 
+    EXCEPTION WHEN SQLSTATE '23502' THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+	_results = _results || assert.pass(full_function_name, test_name);
       WHEN OTHERS THEN 
         GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-	_results =  _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);       
-        RETURN; 
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);         
+        RETURN;
   END;
   RETURN;
 END
