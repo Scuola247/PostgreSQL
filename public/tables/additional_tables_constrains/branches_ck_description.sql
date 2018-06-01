249af2636b9c1ff80b45fd397ebf2dd4357c6d4a
Jiahao <jiahaodong@libero.it>
2018-06-01 12:18:19 +0200

    Aggiunte constrain branches_ck_description.sql e notes_ck_description.sql

---
+++ public/tables/additional_tables_constrains/branches_ck_description.sql
@@ -0,0 +1,6 @@
+﻿-- Check: public.branches_ck_description
+
+-- ALTER TABLE public.branches DROP CONSTRAINT branches_ck_description;
+
+ALTER TABLE public.branches
+  ADD CONSTRAINT branches_ck_description CHECK (length(btrim(description::text)) > 0);
