bd3ba308c34dbd723eb72ba12ab4cc84c7a6fb3f
TzoME <>
2018-06-01 11:40:46 +0200

    vincoli addizionali per unit_tests di Nicholas

---
+++ public/tables/additional_tables_constrains/classrooms_ck_section.sql
@@ -0,0 +1,6 @@
+﻿-- Check: public.classrooms_ck_section
+
+-- ALTER TABLE public.classrooms DROP CONSTRAINT classrooms_ck_section;
+
+ALTER TABLE public.classrooms
+  ADD CONSTRAINT classrooms_ck_section CHECK (length(btrim(section::text)) > 0);
---
+++ public/tables/additional_tables_constrains/notes_ck_description.sql
@@ -0,0 +1,6 @@
+﻿-- Check: public.notes_ck_description
+
+-- ALTER TABLE public.notes DROP CONSTRAINT notes_ck_description;
+
+ALTER TABLE public.notes
+  ADD CONSTRAINT notes_ck_description CHECK (length(btrim(description::text)) > 0);
