0cbd00acc2d4f5cebf831a7e653bab4256769d1e
TzoME <>
2018-06-01 12:30:44 +0200

    additional constraints  fro Nicholas and Jia Hao

---
+++ public/tables/additional_tables_constrains/messages_ck_message_text.sql
@@ -0,0 +1,6 @@
+﻿-- Check: public.messages_ck_message_text
+
+-- ALTER TABLE public.messages DROP CONSTRAINT messages_ck_message_text;
+
+ALTER TABLE public.messages
+  ADD CONSTRAINT messages_ck_message_text CHECK (length(btrim(message_text::text)) > 0);
