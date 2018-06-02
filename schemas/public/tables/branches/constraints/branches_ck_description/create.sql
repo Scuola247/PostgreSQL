c514ad7feb9289b4122b687822fe82649dee06e4
TzoME <>
2018-06-02 17:54:40 +0200

    delete comments

--- schemas/public/tables/branches/constraints/branches_ck_description/create.sql
+++ schemas/public/tables/branches/constraints/branches_ck_description/create.sql
@@ -1,7 +1,2 @@
-﻿-- Check: public.branches_ck_description
-
--- ALTER TABLE public.branches DROP CONSTRAINT branches_ck_description;
--- another comment line
-
-ALTER TABLE public.branches
+﻿ALTER TABLE public.branches
   ADD CONSTRAINT branches_ck_description CHECK (length(btrim(description::text)) > 0);
