1b5cb2e434d4435bc8f6c442a3b7d743ffa48e04
Andrea Adami <fol@fulcro.net>
2018-06-02 17:47:21 +0200

    Prova di un cambiamento che genera un conflitto

--- schemas/public/tables/branches/constraints/branches_ck_description/create.sql
+++ schemas/public/tables/branches/constraints/branches_ck_description/create.sql
@@ -1,6 +1,2 @@
-﻿-- Check: public.branches_ck_description
-
--- ALTER TABLE public.branches DROP CONSTRAINT branches_ck_description;
-
 ALTER TABLE public.branches
   ADD CONSTRAINT branches_ck_description CHECK (length(btrim(description::text)) > 0);
