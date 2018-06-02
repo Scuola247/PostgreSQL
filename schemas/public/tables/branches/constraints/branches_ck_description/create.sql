3657a1c93c58556faf847d72c52e1bffced6a4f5
TzoME <>
2018-06-02 17:48:17 +0200

    aggiunto commento

--- schemas/public/tables/branches/constraints/branches_ck_description/create.sql
+++ schemas/public/tables/branches/constraints/branches_ck_description/create.sql
@@ -1,6 +1,7 @@
 ﻿-- Check: public.branches_ck_description
 
 -- ALTER TABLE public.branches DROP CONSTRAINT branches_ck_description;
+-- another comment line
 
 ALTER TABLE public.branches
   ADD CONSTRAINT branches_ck_description CHECK (length(btrim(description::text)) > 0);
