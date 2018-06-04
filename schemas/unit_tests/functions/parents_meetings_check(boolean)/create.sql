069dd9e3abed38a824bea84ece2f088000830d1a
STEFAN\HP <stefanmilchevski@gmail.com>
2018-06-04 10:26:12 +0200

    aggiunto funzioni modificate

---
+++ schemas/unit_tests/functions/parents_meetings_check(boolean)/create.sql
@@ -0,0 +1,75 @@
+﻿-- Function: unit_tests.parents_meetings_check(boolean)
+
+-- DROP FUNCTION unit_tests.parents_meetings_check(boolean);
+
+CREATE OR REPLACE FUNCTION unit_tests.parents_meetings_check(
+    IN _build_dependencies boolean DEFAULT false,
+    OUT _results unit_testing.unit_test_result[])
+  RETURNS unit_testing.unit_test_result[] AS
+$BODY$
+<<me>>
+DECLARE 
+  context               text;
+  full_function_name 	text;
+  test_name		text = '';
+  error			diagnostic.error;
+BEGIN
+  GET DIAGNOSTICS context = PG_CONTEXT;
+  full_function_name = diagnostic.full_function_name(context);
+  -- check to build dependencies
+  IF _build_dependencies THEN
+      PERFORM unit_testing.build_function_dependencies(diagnostic.function_name(context),'_after_data_insert');
+    RETURN;
+  END IF;  
+
+  ------------------------------------
+  test_name = 'setting parents_meeting null';
+  ------------------------------------
+  BEGIN
+    UPDATE parents_meetings SET parents_meeting= NULL WHERE parents_meeting = '33433000000000';
+    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but parents_meeting was expected', NULL::diagnostic.error);   
+    RETURN;
+    EXCEPTION WHEN SQLSTATE '23502' THEN 
+      _results = _results || assert.pass(full_function_name, test_name);  
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error); 
+        RETURN;
+  END; 
+    ------------------------------------
+  test_name = 'setting parents_meeting null';
+  ------------------------------------
+  BEGIN
+    UPDATE parents_meetings SET teacher= NULL WHERE parents_meeting = '33433000000000';
+    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but teacher was expected', NULL::diagnostic.error);   
+    RETURN;
+    EXCEPTION WHEN SQLSTATE '23502' THEN 
+      _results = _results || assert.pass(full_function_name, test_name);  
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error); 
+        RETURN;
+  END; 
+  ------------------------------------
+  test_name = 'setting parents_meeting null';
+  ------------------------------------
+  BEGIN
+    UPDATE parents_meetings SET teacher =NULL WHERE parents_meeting = '33433000000000';
+    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but teacher was expected', NULL::diagnostic.error);   
+    RETURN;
+    EXCEPTION WHEN SQLSTATE '23502' THEN 
+      _results = _results || assert.pass(full_function_name, test_name);  
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error); 
+        RETURN;
+  END; 
+
+
+  RETURN; 
+END
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION unit_tests.parents_meetings_check(boolean)
+  OWNER TO postgres;
