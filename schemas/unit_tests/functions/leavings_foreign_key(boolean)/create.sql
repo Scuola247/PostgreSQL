b5f24ac3edc32b49f1794a2589fa49c99ffee0dd
Nicholas <mantovanicholas32@gmail.com>
2018-06-04 17:24:10 +0200

    Aggiunto leavings_foreign_key senza errori

--- schemas/unit_tests/functions/leavings_foreign_key(boolean)/create.sql
+++ schemas/unit_tests/functions/leavings_foreign_key(boolean)/create.sql
@@ -27,27 +27,26 @@ BEGIN
                                                                                        'lessons');
     RETURN;
  END IF;  
-  --				TROVATI PROBLEMI 
    ---------------------------------------------------------------------
- -- test_name = 'update schools set classroom_student with non existence code';
+   test_name = 'update schools set classroom_student with non existence code';
   ---------------------------------------------------------------------
-  -- BEGIN
-  --   UPDATE leavings SET classroom_student = '9999999999' WHERE leaving = '58393000000000'; 
-  --   _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but classroom_student set with non existence code', NULL::diagnostic.error);   
-  --   RETURN;
- --   EXCEPTION WHEN SQLSTATE '23503' THEN
-  --     GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-   --    IF error.constraint_name = 'leavings_fk_classrooom_student' THEN
-    --     _results = _results || assert.pass(full_function_name, test_name);
-  --     ELSE
-  --       _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception 1', error);   
-  --       RETURN;
-   --    END IF;
-   --    WHEN OTHERS THEN 
-    --     GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-   --      _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception 2', error);   
-   --      RETURN;
-   --END; 
+   BEGIN
+     UPDATE leavings SET classroom_student = '9999' WHERE leaving = '58393000000000'; 
+     _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but classroom_student set with non existence code', NULL::diagnostic.error);   
+     RETURN;
+    EXCEPTION WHEN SQLSTATE '23503' THEN
+       GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+       IF error.constraint_name = 'leavings_fk_classroom_student' THEN
+         _results = _results || assert.pass(full_function_name, test_name);
+       ELSE
+         _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception 1', error);   
+         RETURN;
+       END IF;
+       WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+         _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception 2', error);   
+         RETURN;
+   END; 
   ---------------------------------------------------------------------
   test_name = 'update leavings set explanation with non existence code';
   ---------------------------------------------------------------------
