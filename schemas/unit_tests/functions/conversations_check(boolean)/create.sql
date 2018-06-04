c29a8e73f16e23a89ebeb513973f14a1de3c586f
Jiahao <jiahaodong@libero.it>
2018-06-04 12:09:32 +0200

    Aggiunta funzione unit_tests.conversations_check(boolean)

---
+++ schemas/unit_tests/functions/conversations_check(boolean)/create.sql
@@ -0,0 +1,103 @@
+﻿-- Function: unit_tests.conversations_check(boolean)
+
+-- DROP FUNCTION unit_tests.conversations_check(boolean);
+
+CREATE OR REPLACE FUNCTION unit_tests.conversations_check(
+    IN _build_dependencies boolean DEFAULT false,
+    OUT _results unit_testing.unit_test_result[])
+  RETURNS unit_testing.unit_test_result[] AS
+$BODY$
+<<me>>
+DECLARE 
+  context               text;
+  full_function_name 	text;
+  test_name		text = '';
+  error			diagnostic.error;
+BEGIN
+  GET DIAGNOSTICS context = PG_CONTEXT;
+  full_function_name = diagnostic.full_function_name(context);
+  -- check to build dependencies
+  IF _build_dependencies THEN
+      PERFORM unit_testing.build_function_dependencies(diagnostic.function_name(context),'_after_data_insert');
+    RETURN;
+  END IF;
+  
+  ------------------------------------
+  test_name = 'classroom_student mandatory';
+  ------------------------------------
+  BEGIN
+    UPDATE conversations SET classroom_student = NULL WHERE conversation = '33670000000000';
+    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but classroom_student required was expected', NULL::diagnostic.error);     
+    RETURN;    
+    EXCEPTION WHEN SQLSTATE '23502' THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+	_results = _results || assert.pass(full_function_name, test_name);
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);         
+        RETURN;
+  END; 
+  ---------------------------------
+  test_name = 'subject mandatory';
+  ---------------------------------
+  BEGIN
+    UPDATE conversations SET subject = NULL WHERE conversation = '33670000000000';
+    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but subject required was expected', NULL::diagnostic.error);     
+    RETURN;    
+    EXCEPTION WHEN SQLSTATE '23502' THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+	_results = _results || assert.pass(full_function_name, test_name);
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);         
+        RETURN;
+  END; 
+
+  ----------------------------------------
+  test_name = 'subject''s min lenght';
+  ----------------------------------------
+  BEGIN
+    UPDATE conversations SET subject = '' WHERE conversation = '33670000000000';
+    _results = _results ||  assert.fail(full_function_name, test_name, 'Update was OK but subject min lenght was expected', NULL::diagnostic.error);    
+    RETURN;   
+    EXCEPTION WHEN SQLSTATE '23514' THEN 
+      GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+      IF error.constraint_name = 'conversations_ck_subject' THEN
+        _results = _results || assert.pass(full_function_name, test_name);
+     ELSE
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);       
+        RETURN;
+      END IF;    
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);   
+        RETURN;
+  END; 
+
+  ----------------------------------------
+  test_name = 'end_on begin_on check';
+  ----------------------------------------
+  BEGIN
+    UPDATE conversations SET begin_on = '2013-10-25 10:44:59' , end_on = '2013-10-24 10:44:59' WHERE conversation = '33670000000000';
+    _results = _results ||  assert.fail(full_function_name, test_name, 'Update was OK but begin_on bigger than end_on was expected', NULL::diagnostic.error);    
+    RETURN;   
+    EXCEPTION WHEN SQLSTATE '23514' THEN 
+      GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+      IF error.constraint_name = 'conversations_ck_end_on' THEN
+        _results = _results || assert.pass(full_function_name, test_name);
+     ELSE
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);       
+        RETURN;
+      END IF;    
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);   
+        RETURN;
+  END; 
+  RETURN; 
+END
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION unit_tests.conversations_check(boolean)
+  OWNER TO postgres;
