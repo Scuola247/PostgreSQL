6067c19c7a8329ccebd8d8ac8fb423479cede0a2
Aimen <Aimen@github.com>
2018-06-05 17:00:38 +0200

    Aggiunti funzioni assegnate

---
+++ schemas/unit_tests/functions/school_years_check(boolean)/create.sql
@@ -0,0 +1,152 @@
+﻿-- Function: unit_tests.schools_years_check(boolean)
+
+-- DROP FUNCTION unit_tests.schools_years_check(boolean);
+
+CREATE OR REPLACE FUNCTION unit_tests.schools_years_check(
+    IN _build_dependencies boolean DEFAULT false,
+    OUT _results unit_testing.unit_test_result[])
+  RETURNS unit_testing.unit_test_result[] AS
+$BODY$
+<<me>>
+DECLARE 
+  context               text;
+  full_function_name 	text;
+  test_name		text = '';
+  error			diagnostic.error;
+BEGIN
+  GET DIAGNOSTICS context = PG_CONTEXT;
+  full_function_name = diagnostic.full_function_name(context);
+  -- check to build dependencies
+  IF _build_dependencies THEN
+      PERFORM unit_testing.build_function_dependencies(diagnostic.function_name(context),'_after_data_insert');
+    RETURN;
+  END IF;
+  ---------------------------------------
+  test_name = 'school''s mandatory';
+  ---------------------------------------
+  BEGIN
+    UPDATE school_years  SET school = NULL WHERE school_year = '244000000000';
+    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but school required was expected', NULL::diagnostic.error);     
+    RETURN;    
+    EXCEPTION WHEN SQLSTATE '23502' THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+	_results = _results || assert.pass(full_function_name, test_name);
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);         
+        RETURN;
+  END;
+
+  ---------------------------------------
+  test_name = 'description''s mandatory';
+  ---------------------------------------
+  BEGIN
+    UPDATE school_years  SET description = NULL WHERE school_year = '244000000000';
+    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but description required was expected', NULL::diagnostic.error);     
+    RETURN;    
+    EXCEPTION WHEN SQLSTATE '23502' THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+	_results = _results || assert.pass(full_function_name, test_name);
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);         
+        RETURN;
+  END;
+
+
+  ------------------------------------
+  test_name = 'duplicate description';
+  ------------------------------------
+  BEGIN
+    INSERT INTO school_years (school_year, school, description, duration, lessons_duration) VALUES (1244000000000, 2000000000, '2013/2014', '[2014-09-11,2015-09-11)', '[2014-09-11,2015-06-08)');
+   _results =  _results || assert.fail(full_function_name, test_name, 'Insert was OK but duplicate description was expected', NULL::diagnostic.error);      
+    RETURN;      
+
+    EXCEPTION WHEN SQLSTATE '23505' THEN 
+      GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;	
+      IF error.constraint_name = 'school_years_uq_description' THEN
+        _results =  _results || assert.pass(full_function_name, test_name);
+      ELSE
+	_results =  _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);      
+	RETURN; 
+      END IF; 
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+	_results =  _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);       
+        RETURN; 
+  END;
+
+
+  ------------------------------------
+  test_name = 'check duration';
+  ------------------------------------
+  BEGIN
+    INSERT INTO school_years (school_year, school, description, duration, lessons_duration) VALUES (1244000000000, 2000000000, '2013/2014', '[2013-09-11,2014-05-11)', '[2013-09-11,2014-06-08)');
+   _results =  _results || assert.fail(full_function_name, test_name, 'UPDATE was OK but duration is out of range', NULL::diagnostic.error);      
+    RETURN;      
+
+    EXCEPTION WHEN SQLSTATE '23514' THEN 
+      GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;	
+      IF error.constraint_name = 'school_years_ck_duration' THEN
+        _results =  _results || assert.pass(full_function_name, test_name);
+      ELSE
+	_results =  _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);      
+	RETURN; 
+      END IF; 
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+	_results =  _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);       
+        RETURN; 
+  END;
+
+
+  ------------------------------------
+  test_name = 'check duration overlap';
+  ------------------------------------
+  BEGIN
+    UPDATE school_years SET description = '2014/2015', school = '28961000000000' WHERE school_year = '244000000000';
+   _results =  _results || assert.fail(full_function_name, test_name, 'UPDATE was OK but duration overlaps', NULL::diagnostic.error);      
+    RETURN;      
+
+    EXCEPTION WHEN SQLSTATE '23P01' THEN 
+      GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;	
+      IF error.constraint_name = 'school_years_ex_duration' THEN
+        _results =  _results || assert.pass(full_function_name, test_name);
+      ELSE
+	_results =  _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);      
+	RETURN; 
+      END IF; 
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+	_results =  _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);       
+        RETURN; 
+  END;
+
+  ----------------------------------------
+  test_name = 'description''s min lenght';
+  ----------------------------------------
+  BEGIN
+    UPDATE school_years SET description = '' WHERE school_year = '244000000000';
+    _results = _results ||  assert.fail(full_function_name, test_name, 'Update was OK but description min lenght was expected', NULL::diagnostic.error);    
+    RETURN;   
+    EXCEPTION WHEN SQLSTATE '23514' THEN 
+      GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+      IF error.constraint_name = 'school_years_ck_description' THEN
+        _results = _results || assert.pass(full_function_name, test_name);
+     ELSE
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);       
+        RETURN;
+      END IF;    
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);   
+        RETURN;
+  END; 
+  
+  RETURN; 
+END
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION unit_tests.schools_years_check(boolean)
+  OWNER TO postgres;
---
+++ schemas/unit_tests/functions/wikimedia_files_persons_foreign_key(boolean)/create.sql
@@ -0,0 +1,74 @@
+﻿-- Function: unit_tests.valutations_foreign_key(boolean)
+
+-- DROP FUNCTION unit_tests.valutations_foreign_key(boolean);
+
+CREATE OR REPLACE FUNCTION unit_tests.valutations_foreign_key(
+    IN _build_dependencies boolean DEFAULT false,
+    OUT _results unit_testing.unit_test_result[])
+  RETURNS unit_testing.unit_test_result[] AS
+$BODY$
+<<me>>
+DECLARE 
+  context               text;
+  full_function_name 	text;
+  test_name		text = '';
+  error			diagnostic.error;
+BEGIN
+  GET DIAGNOSTICS context = PG_CONTEXT;
+  full_function_name = diagnostic.full_function_name(context);
+  -- check to build dependencies
+  IF _build_dependencies THEN
+    PERFORM unit_testing.build_function_dependencies(diagnostic.function_name(context),'_after_data_insert');
+    RETURN;
+  END IF;  
+  --------------------------------------------------------------
+  test_name = 'UPDATE wikimedia_files_persons with a non existing person';
+  --------------------------------------------------------------
+  BEGIN
+    UPDATE public.wikimedia_files_persons set person = '99999999999999' WHERE wikimedia_file_person = '329668000000000';
+    _results = _results || assert.fail(full_function_name, test_name,'UPDATE was OK but the person does not exist ', NULL::diagnostic.error);
+    RETURN;
+    EXCEPTION WHEN SQLSTATE '23503' THEN
+    GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+      IF error.constraint_name = 'wikimedia_files_persons_fk_person' THEN
+        _results = _results || assert.pass(full_function_name, test_name);
+      ELSE
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception 1', error);   
+        RETURN;
+      END IF;
+      
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error); 
+        RETURN;
+  END;
+
+  --------------------------------------------------------------
+  test_name = 'UPDATE wikimedia_files_persons with a non existing wikimedia_file';
+  --------------------------------------------------------------
+  BEGIN
+    UPDATE public.wikimedia_files_persons set wikimedia_file = '99999999999999' WHERE wikimedia_file_person = '329668000000000';
+    _results = _results || assert.fail(full_function_name, test_name,'UPDATE was OK but the wikimedia_file does not exist ', NULL::diagnostic.error);
+    RETURN;
+    EXCEPTION WHEN SQLSTATE '23503' THEN
+    GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+      IF error.constraint_name = 'wikimedia_files_persons_fk_wikimedia_file' THEN
+        _results = _results || assert.pass(full_function_name, test_name);
+      ELSE
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception 1', error);   
+        RETURN;
+      END IF;
+      
+      WHEN OTHERS THEN 
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error); 
+        RETURN;
+  END;
+   
+  RETURN; 
+END
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION unit_tests.valutations_foreign_key(boolean)
+  OWNER TO postgres;
