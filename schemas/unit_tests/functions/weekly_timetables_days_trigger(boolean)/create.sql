60b49851ab45894a044b244c23c38028e5054ed2
Andrea Adami <fol@fulcro.net>
2018-06-05 15:44:38 +0200

    Use the new asset.sqlstateequal

--- schemas/unit_tests/functions/weekly_timetables_days_trigger(boolean)/create.sql
+++ schemas/unit_tests/functions/weekly_timetables_days_trigger(boolean)/create.sql
@@ -28,12 +28,19 @@ BEGIN
     UPDATE weekly_timetables_days SET  teacher='287713000000000'  WHERE weekly_timetable_day = '33008000000000';  
     _results = _results || assert.fail(full_function_name, test_name,'Update was OK but the teacher is from different school', NULL::diagnostic.error);
     RETURN;
+ /*   
     EXCEPTION WHEN SQLSTATE 'U0591' THEN
       _results = _results || assert.pass(full_function_name, test_name);
       WHEN OTHERS THEN 
         GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
         _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error); 
         RETURN;
+*/
+  EXCEPTION WHEN OTHERS THEN
+    GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;	
+    _results = _results || assert.sqlstate_equals(me.full_function_name, me.test_name, me.error, 'U0591');
+    IF (_results[array_length(_results,1)]).check_point.status = 'Failed' THEN RETURN; END IF;
+        
   END;  
   -----------------------------------------------------------------------------
   test_name = 'UPDATE weekly_timetables_days set subject with different school';
