61bc75217c956d25fe4bc1693f8e26ad4cdbb16a
Jiahao <jiahaodong@libero.it>
2018-06-06 17:50:01 +0200

    Aggiunto unit_tests_public_persons_addresses(boolean) e aggiornato i suoi check e foreign_key

--- schemas/unit_tests/functions/persons_addresses_check(boolean)/create.sql
+++ schemas/unit_tests/functions/persons_addresses_check(boolean)/create.sql
@@ -1,8 +1,8 @@
-﻿-- Function: unit_tests.persons_addresses_check(boolean)
+﻿-- Function: unit_tests_public.persons_addresses_check(boolean)
 
--- DROP FUNCTION unit_tests.persons_addresses_check(boolean);
+-- DROP FUNCTION unit_tests_public.persons_addresses_check(boolean);
 
-CREATE OR REPLACE FUNCTION unit_tests.persons_addresses_check(
+CREATE OR REPLACE FUNCTION unit_tests_public.persons_addresses_check(
     IN _build_dependencies boolean DEFAULT false,
     OUT _results unit_testing.unit_test_result[])
   RETURNS unit_testing.unit_test_result[] AS
@@ -18,7 +18,7 @@ BEGIN
   full_function_name = diagnostic.full_function_name(context);
   -- check to build dependencies
   IF _build_dependencies THEN
-      PERFORM unit_testing.build_function_dependencies(diagnostic.function_name(context),'_after_data_insert');
+      PERFORM unit_testing.build_function_dependencies(diagnostic.full_function_name(context),'unit_tests_public._after_data_insert');
     RETURN;
   END IF;
   --    INSERT INTO public.persons_addresses(person_address,person,address_type,street,zip_code,city) VALUES ('16765000000000','3326000000000','Residence','Via G. Segantini 91','37069','L949');
@@ -187,5 +187,5 @@ END
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION unit_tests.persons_addresses_check(boolean)
+ALTER FUNCTION unit_tests_public.persons_addresses_check(boolean)
   OWNER TO postgres;
