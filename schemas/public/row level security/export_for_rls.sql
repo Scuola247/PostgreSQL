b5f3446cfd60f838b31513cee1afbdf9de37c145
Andrea Adami <fol@fulcro.net>
2018-06-06 13:10:09 +0200

    Added row level security

---
+++ schemas/public/row level security/export_for_rls.sql
@@ -0,0 +1,4 @@
+﻿COPY (SELECT * FROM public.usenames_schools) TO '/tmp/usenames_schoosl.csv' WITH CSV;
+COPY public.usenames_schools FROM '/tmp/usenames_schoosl.csv'  WITH CSV;
+COPY (SELECT usename FROM pg_user) TO '/tmp/usenames.csv';
+VACUUM FULL VERBOSE ANALYZE absences
