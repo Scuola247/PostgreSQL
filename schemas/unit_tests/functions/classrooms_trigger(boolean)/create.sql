6bf2a941b2d99d598e57d8724ed44da029a65d52
Federico Tomelleri <federico.tomelleri@gmail.com>
2018-06-07 18:52:21 +0200

    review

--- schemas/unit_tests/functions/classrooms_trigger(boolean)/create.sql
+++ schemas/unit_tests/functions/classrooms_trigger(boolean)/create.sql
@@ -19,8 +19,8 @@ BEGIN
   -- check to build dependencies
   IF _build_dependencies THEN
     PERFORM unit_testing.build_function_dependencies(diagnostic.function_name(context),'unit_tests_public.school_years',
-										       'unit_tests_public.degrees',
-										       'unit_tests_public.branches');
+										       										       										               'unit_tests_public.degrees',
+														       										              						       'unit_tests_public.branches');
     RETURN;
   END IF;
   ---------------------------------------------------------------------
--- schemas/unit_tests/functions/communication_types_check(boolean)/create.sql
+++ schemas/unit_tests/functions/communication_types_check(boolean)/create.sql
@@ -21,23 +21,7 @@ BEGIN
       PERFORM unit_testing.build_function_dependencies(diagnostic.function_name(context),'unit_tests_public._after_data_insert');
     RETURN;
   END IF;
-/*
-  ----------------------------------------------
-  test_name = 'communication_type''s mandatory';
-  ----------------------------------------------
-  BEGIN
-    UPDATE communication_types SET communication_type = NULL WHERE communication_type = 138012000000000;
-    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but communication_type required was expected', NULL::diagnostic.error);
-    RETURN;
-    EXCEPTION WHEN SQLSTATE '23502' THEN
-        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-	_results = _results || assert.pass(full_function_name, test_name);
-      WHEN OTHERS THEN
-        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
-        RETURN;
-  END;
-  */
+
   ---------------------------------------
   test_name = 'description''s mandatory';
   ---------------------------------------
@@ -53,25 +37,9 @@ BEGIN
         _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
         RETURN;
   END;
-  /*
-  ---------------------------------------------------
-  test_name = 'notification_management''s mandatory';
-  ---------------------------------------------------
-  BEGIN
-    UPDATE communication_types SET notification_management = NULL WHERE communication_type = 138012000000000;
-    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but notification_management required was expected', NULL::diagnostic.error);
-    RETURN;
-    EXCEPTION WHEN SQLSTATE '23502' THEN
-        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-	_results = _results || assert.pass(full_function_name, test_name);
-      WHEN OTHERS THEN
-        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
-        RETURN;
-  END;
-  */
+
   --------------------------------------
-  test_name = 'desccription min lenght';
+  test_name = 'description min lenght';
   --------------------------------------
   BEGIN
     UPDATE communication_types SET description = '   ' WHERE communication_type =138012000000000 ;
