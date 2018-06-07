d3d3dc57aaa07505e592e443021db677f77bde54
Federico Tomelleri <federico.tomelleri@gmail.com>
2018-06-07 23:42:51 +0200

    review

--- schemas/unit_tests/functions/degrees_foreign_key(boolean)/create.sql
+++ schemas/unit_tests/functions/degrees_foreign_key(boolean)/create.sql
@@ -8,11 +8,11 @@ CREATE OR REPLACE FUNCTION unit_tests_public.degrees_foreign_key(
   RETURNS unit_testing.unit_test_result[] AS
 $BODY$
 <<me>>
-DECLARE 
+DECLARE
   context               text;
-  full_function_name 	text;
-  test_name		text = '';
-  error			diagnostic.error;
+  full_function_name 	  text;
+  test_name		          text = '';
+  error		             	diagnostic.error;
 BEGIN
   GET DIAGNOSTICS context = PG_CONTEXT;
   full_function_name = diagnostic.full_function_name(context);
@@ -20,29 +20,29 @@ BEGIN
   IF _build_dependencies THEN
     PERFORM unit_testing.build_function_dependencies(diagnostic.function_name(context),'unit_tests_public._after_data_insert');
     RETURN;
-  END IF;  
-  --------------------------------------------------------------
+  END IF;
+  --------------------------------------------------------
   test_name = 'UPDATE degrees with a non existing school';
-  --------------------------------------------------------------
+  --------------------------------------------------------
   BEGIN
     UPDATE public.degrees set school = '99999999999999' WHERE degree = '9942000000000';
-    _results = _results || assert.fail(full_function_name, test_name,'UPDATE was OK but the message does not exist ', NULL::diagnostic.error);
+    _results = _results || assert.fail(full_function_name, test_name,'UPDATE was OK but the school does not exist ', NULL::diagnostic.error);
     RETURN;
     EXCEPTION WHEN SQLSTATE '23503' THEN
     GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
       IF error.constraint_name = 'degrees_fk_school' THEN
         _results = _results || assert.pass(full_function_name, test_name);
       ELSE
-        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception 1', error);   
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception 1', error);
         RETURN;
       END IF;
-      
-      WHEN OTHERS THEN 
+
+      WHEN OTHERS THEN
         GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error); 
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
         RETURN;
   END;
-  RETURN; 
+  RETURN;
 END
 $BODY$
   LANGUAGE plpgsql VOLATILE
