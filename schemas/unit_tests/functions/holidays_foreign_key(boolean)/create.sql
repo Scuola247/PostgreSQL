65c43efcdfe8b9e8ecf7a38b5e40bf722ba5e025
Federico Tomelleri <federico.tomelleri@gmail.com>
2018-06-08 18:22:13 +0200

    review

--- schemas/unit_tests/functions/holidays_foreign_key(boolean)/create.sql
+++ schemas/unit_tests/functions/holidays_foreign_key(boolean)/create.sql
@@ -8,11 +8,11 @@ CREATE OR REPLACE FUNCTION unit_tests_public.holidays_foreign_key(
   RETURNS unit_testing.unit_test_result[] AS
 $BODY$
 <<me>>
-DECLARE 
+DECLARE
   context               text;
-  full_function_name 	text;
-  test_name		text = '';
-  error			diagnostic.error;
+  full_function_name 	  text;
+  test_name		          text = '';
+  error			            diagnostic.error;
 BEGIN
   GET DIAGNOSTICS context = PG_CONTEXT;
   full_function_name = diagnostic.full_function_name(context);
@@ -20,28 +20,28 @@ BEGIN
   IF _build_dependencies THEN
     PERFORM unit_testing.build_function_dependencies(diagnostic.function_name(context),'unit_tests_public._after_data_insert');
     RETURN;
-  END IF;  
-  ---------------------------------------------------------------------------
-  test_name = 'update holidays set school with a non existing one';
-  ---------------------------------------------------------------------------
+  END IF;
+  ---------------------------------------------------------------
+  test_name = 'update holidays set school to a non existing one';
+  ---------------------------------------------------------------
   BEGIN
-    UPDATE holidays SET school = 999999999 WHERE holiday = '11335000000000'; 
-    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but school set with a non existing one', NULL::diagnostic.error);   
+    UPDATE holidays SET school = 999999999 WHERE holiday = '11335000000000';
+    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but school set to a non existing one', NULL::diagnostic.error);
     RETURN;
     EXCEPTION WHEN SQLSTATE '23503' THEN
       GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
       IF error.constraint_name = 'holidays_fk_school' THEN
         _results = _results || assert.pass(full_function_name, test_name);
       ELSE
-        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception 1', error);   
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception 1', error);
         RETURN;
       END IF;
-      WHEN OTHERS THEN 
+      WHEN OTHERS THEN
         GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception 2', error);   
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception 2', error);
         RETURN;
-  END; 
-  RETURN; 
+  END;
+  RETURN;
 END
 $BODY$
   LANGUAGE plpgsql VOLATILE
