92f5b07c54fde60a8662c8c923cf0fbd4c242bd7
Federico Tomelleri <federico.tomelleri@gmail.com>
2018-06-11 10:20:29 +0200

    review, messages check da completare, vedere commenti nel file per maggiori info

--- schemas/unit_tests/functions/messages_check(boolean)/create.sql
+++ schemas/unit_tests/functions/messages_check(boolean)/create.sql
@@ -8,11 +8,11 @@ CREATE OR REPLACE FUNCTION unit_tests_public.messages_check(
   RETURNS unit_testing.unit_test_result[] AS
 $BODY$
 <<me>>
-DECLARE 
+DECLARE
   context               text;
-  full_function_name 	text;
-  test_name		text = '';
-  error			diagnostic.error;
+  full_function_name 	  text;
+  test_name		          text = '';
+  error			            diagnostic.error;
 BEGIN
   GET DIAGNOSTICS context = PG_CONTEXT;
   full_function_name = diagnostic.full_function_name(context);
@@ -20,99 +20,77 @@ BEGIN
   IF _build_dependencies THEN
       PERFORM unit_testing.build_function_dependencies(diagnostic.function_name(context),'unit_tests_public._after_data_insert');
     RETURN;
-  END IF;  
-  ------------------------------------
-  test_name = 'message set NULL';
-  ------------------------------------
-  BEGIN
-    UPDATE messages SET message= NULL WHERE message = '50112000000000';
-    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but mnemonic mandatory was expected', NULL::diagnostic.error);   
-    RETURN;
-    EXCEPTION WHEN SQLSTATE '23502' THEN 
-      _results = _results || assert.pass(full_function_name, test_name);  
-      WHEN OTHERS THEN 
-        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error); 
-        RETURN;
-  END; 
+  END IF;
+
   ------------------------------------
   test_name = 'conversation set NULL';
   ------------------------------------
   BEGIN
     UPDATE messages SET conversation= NULL WHERE message = '50112000000000';
-    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but mnemonic mandatory was expected', NULL::diagnostic.error);   
+    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but mnemonic mandatory was expected', NULL::diagnostic.error);
     RETURN;
-    EXCEPTION WHEN SQLSTATE '23502' THEN 
-      _results = _results || assert.pass(full_function_name, test_name);  
-      WHEN OTHERS THEN 
+    EXCEPTION WHEN SQLSTATE '23502' THEN
+      _results = _results || assert.pass(full_function_name, test_name);
+      WHEN OTHERS THEN
         GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error); 
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
         RETURN;
-  END; 
-  ------------------------------------
-  test_name = 'written_on set NULL';
-  ------------------------------------
-  BEGIN
-    UPDATE messages SET written_on= NULL WHERE message = '50112000000000';
-    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but mnemonic mandatory was expected', NULL::diagnostic.error);   
-    RETURN;
-    EXCEPTION WHEN SQLSTATE '23502' THEN 
-      _results = _results || assert.pass(full_function_name, test_name);  
-      WHEN OTHERS THEN 
-        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error); 
-        RETURN;
-  END; 
+  END;
+
   ------------------------------------
   test_name = 'message_text set NULL';
   ------------------------------------
   BEGIN
     UPDATE messages SET message_text= NULL WHERE message = '50112000000000';
-    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but mnemonic mandatory was expected', NULL::diagnostic.error);   
+    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but mnemonic mandatory was expected', NULL::diagnostic.error);
     RETURN;
-    EXCEPTION WHEN SQLSTATE '23502' THEN 
-      _results = _results || assert.pass(full_function_name, test_name);  
-      WHEN OTHERS THEN 
+    EXCEPTION WHEN SQLSTATE '23502' THEN
+      _results = _results || assert.pass(full_function_name, test_name);
+      WHEN OTHERS THEN
         GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error); 
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
         RETURN;
-  END; 
-  ------------------------------------
+  END;
+  ------------------------------
   test_name = 'person set NULL';
-  ------------------------------------
+  ------------------------------
   BEGIN
     UPDATE messages SET person= NULL WHERE message = '50112000000000';
-    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but mnemonic mandatory was expected', NULL::diagnostic.error);   
+    _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but mnemonic mandatory was expected', NULL::diagnostic.error);
     RETURN;
-    EXCEPTION WHEN SQLSTATE '23502' THEN 
-      _results = _results || assert.pass(full_function_name, test_name);  
-      WHEN OTHERS THEN 
+    EXCEPTION WHEN SQLSTATE '23502' THEN
+      _results = _results || assert.pass(full_function_name, test_name);
+      WHEN OTHERS THEN
         GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error); 
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
         RETURN;
-  END; 
+  END;
 ----------------------------------------
   test_name = 'messages text''s min lenght';
   ----------------------------------------
   BEGIN
     UPDATE messages SET message_text = '   ' WHERE message = '50112000000000';
-    _results = _results ||  assert.fail(full_function_name, test_name, 'Update was OK but messages text min lenght was expected', NULL::diagnostic.error);    
-    RETURN;   
-    EXCEPTION WHEN SQLSTATE '23514' THEN 
+    _results = _results ||  assert.fail(full_function_name, test_name, 'Update was OK but messages text empty was expected', NULL::diagnostic.error);
+    RETURN;
+    EXCEPTION WHEN SQLSTATE '23514' THEN
       GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
       IF error.constraint_name = 'messages_ck_message_text' THEN
         _results = _results || assert.pass(full_function_name, test_name);
      ELSE
-        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);       
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
         RETURN;
-      END IF;    
-      WHEN OTHERS THEN 
+      END IF;
+      WHEN OTHERS THEN
         GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
-        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);   
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
         RETURN;
-  END; 
+  END;
 
-  RETURN; 
+  /*
+manca check duplicate from_time
+  */
+
+  RETURN;
 END
 $BODY$
   LANGUAGE plpgsql VOLATILE
