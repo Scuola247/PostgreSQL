53fc7f03318a16bf1ce6aa26fd0cdc4b8e2e6196
Aimen <Aimen@github.com>
2018-06-11 14:47:09 +0200

    Completato parents_meeting_check(boolean)

--- schemas/unit_tests/functions/parents_meetings_check(boolean)/create.sql
+++ schemas/unit_tests/functions/parents_meetings_check(boolean)/create.sql
@@ -37,10 +37,44 @@ BEGIN
         RETURN;
   END;
 
-/*
-manca check generico:
-- parents_meeting_uq 
-*/
+  ------------------------------------
+
+  test_name = 'duplicate on_date';
+
+  ------------------------------------
+
+  BEGIN
+
+    UPDATE parents_meetings SET on_date = '2013-10-03 00:00:00' WHERE parents_meeting = '33433000000000';
+   _results =  _results || assert.fail(full_function_name, test_name, 'Insert was OK but duplicate on_date was expected', NULL::diagnostic.error);      
+
+    RETURN;      
+
+   EXCEPTION WHEN SQLSTATE '23505' THEN 
+
+      GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;	
+
+      IF error.constraint_name = 'parents_meetings_uq' THEN
+
+        _results =  _results || assert.pass(full_function_name, test_name);
+
+      ELSE
+
+	_results =  _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);      
+
+	RETURN; 
+
+      END IF; 
+
+      WHEN OTHERS THEN 
+
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+
+	_results =  _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);       
+
+        RETURN; 
+
+  END;
 
   RETURN;
 END
