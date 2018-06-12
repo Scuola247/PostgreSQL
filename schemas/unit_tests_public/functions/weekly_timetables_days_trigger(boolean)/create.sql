7505c3e94a3faa88f68f57e6b7f361c56aed6df7
Aimen <Aimen@github.com>
2018-06-12 22:36:53 +0200

    Aggiunto le due insert mancanti.

--- schemas/unit_tests_public/functions/weekly_timetables_days_trigger(boolean)/create.sql
+++ schemas/unit_tests_public/functions/weekly_timetables_days_trigger(boolean)/create.sql
@@ -22,10 +22,6 @@ BEGIN
     RETURN;
   END IF;
 
-/*
-mancano le due insert
-*/
-
   -----------------------------------------------------------------------------
   test_name = 'UPDATE weekly_timetables_days set teacher with different school';
   -----------------------------------------------------------------------------
@@ -47,6 +43,31 @@ mancano le due insert
     IF (_results[array_length(_results,1)]).check_point.status = 'Failed' THEN RETURN; END IF;
 
   END;
+
+
+  -----------------------------------------------------------------------------
+  test_name = 'INSERT weekly_timetables_days set teacher with different school';
+  -----------------------------------------------------------------------------
+  BEGIN
+    INSERT INTO public.weekly_timetables_days(weekly_timetable_day,weekly_timetable,weekday,teacher,subject,team_teaching,from_time,to_time) VALUES ('33008000000000','51386000000000','1','29122000000000','32911000000000','1','08:00:00','09:00:00');
+    _results = _results || assert.fail(full_function_name, test_name,'INSERT was OK but the teacher is from different school', NULL::diagnostic.error);
+    RETURN;
+ /*
+    EXCEPTION WHEN SQLSTATE 'U0591' THEN
+      _results = _results || assert.pass(full_function_name, test_name);
+      WHEN OTHERS THEN
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
+        RETURN;
+*/
+  EXCEPTION WHEN OTHERS THEN
+    GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+    _results = _results || assert.sqlstate_equals(me.full_function_name, me.test_name, me.error, 'U0592');
+    IF (_results[array_length(_results,1)]).check_point.status = 'Failed' THEN RETURN; END IF;
+
+  END;
+
+  
   -----------------------------------------------------------------------------
   test_name = 'UPDATE weekly_timetables_days set subject with different school';
   -----------------------------------------------------------------------------
@@ -60,6 +81,28 @@ mancano le due insert
         GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
         _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
         RETURN;
+  END;
+
+  -----------------------------------------------------------------------------
+  test_name = 'INSERT weekly_timetables_days set subject with different school';
+  -----------------------------------------------------------------------------
+  BEGIN
+    INSERT INTO public.weekly_timetables_days(weekly_timetable_day,weekly_timetable,weekday,teacher,subject,team_teaching,from_time,to_time) VALUES ('33008000000000','51386000000000','1','32925000000000','29105000000000','1','08:00:00','09:00:00');
+    _results = _results || assert.fail(full_function_name, test_name,'INSERT was OK but the subject is from different school', NULL::diagnostic.error);
+    RETURN;
+ /*
+    EXCEPTION WHEN SQLSTATE 'U0591' THEN
+      _results = _results || assert.pass(full_function_name, test_name);
+      WHEN OTHERS THEN
+        GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+        _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
+        RETURN;
+*/
+  EXCEPTION WHEN OTHERS THEN
+    GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+    _results = _results || assert.sqlstate_equals(me.full_function_name, me.test_name, me.error, 'U0594');
+    IF (_results[array_length(_results,1)]).check_point.status = 'Failed' THEN RETURN; END IF;
+
   END;
   RETURN;
 END
