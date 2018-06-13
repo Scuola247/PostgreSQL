7900c0f7853d5c6a18499c06181759a0cc057f71
Andrea Adami <fol@fulcro.net>
2018-06-13 09:14:12 +0200

    Rename

---
+++ Sicurezza orrizontale/switching_users.sql
@@ -0,0 +1,7 @@
+﻿SET ROLE 'test-manager-a@scuola-1.it'
+SET ROLE 'manager-c@scuola-2.it'
+SET ROLE 'manager-e@scuola-28961.it'
+SET ROLE 'fol@scuola247.it'
+SET ROLE 'matteonovelli.my@gmail.com'
+RESET ROLE
+SELECT school, usename from persons WHERE usename IS not null
