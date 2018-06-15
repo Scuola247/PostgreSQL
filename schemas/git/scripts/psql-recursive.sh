f7483d108c521bbaecc009a52cc11511409760fe
Andrea Adami <fol@fulcro.net>
2018-06-15 10:29:10 +0200

    Aggiunto nome del file da lanciare come primo parametro
    Aggiunto come parametro il nome del file sql (quindi non si deve indicare l'estensione) da eseguire

--- schemas/git/scripts/psql-recursive.sh
+++ schemas/git/scripts/psql-recursive.sh
@@ -2,4 +2,4 @@
 # 
 # cerca ed esegue i file indicati dall'opzione -name a partire dal path indicato come primo parametro
 #
-find ./ -name create.sql -exec psql -h postgresql -U postgres -d scuola247 -f {} ';'
+find ./ -name $1.sql -exec psql -h postgresql -U postgres -d scuola247 -f {} ';'
