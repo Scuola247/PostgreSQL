be65c39bda0f090fa75c21cfcf13693ba2bcab60
Jacopo <jack.tognella@gmail.com>
2018-06-15 17:22:05 +0200

    update exception parents_meetings e out_of_classroom_foreign_key

--- schemas/unit_tests_public/functions/parents_meetings_check(boolean)/create.sql
+++ schemas/unit_tests_public/functions/parents_meetings_check(boolean)/create.sql
@@ -29,17 +29,21 @@ BEGIN
     UPDATE parents_meetings SET teacher= NULL WHERE parents_meeting = '33433000000000';
     _results = _results || assert.fail(full_function_name, test_name, 'Update was OK but teacher was expected', NULL::diagnostic.error);
     RETURN;
-    EXCEPTION WHEN SQLSTATE '23502' THEN
+    /*EXCEPTION WHEN SQLSTATE '23502' THEN
       _results = _results || assert.pass(full_function_name, test_name);
       WHEN OTHERS THEN
         GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
         _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
-        RETURN;
+        RETURN;*/
+    EXCEPTION WHEN OTHERS THEN
+     GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+     _results = _results || assert.sqlstate_equals(me.full_function_name, me.test_name, me.error, '23502');
+     IF (_results[array_length(_results,1)]).check_point.status = 'Failed' THEN RETURN; END IF;
   END;
 
 /*
 manca una combinazione sul check parents_meetings_uq
-- duplicate teacher - gia' presente
+- duplicate teacher
 */
 
   ------------------------------------
@@ -55,7 +59,7 @@ manca una combinazione sul check parents_meetings_uq
 
     RETURN;
 
-   EXCEPTION WHEN SQLSTATE '23505' THEN
+   /*EXCEPTION WHEN SQLSTATE '23505' THEN
 
       GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
 
@@ -77,7 +81,11 @@ manca una combinazione sul check parents_meetings_uq
 
 	_results =  _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
 
-        RETURN;
+        RETURN;*/
+     EXCEPTION WHEN OTHERS THEN
+     GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+     _results = _results || assert.sqlstate_constraint_equals(me.full_function_name, me.test_name, me.error, '23505','parents_meetings_uq');
+     IF (_results[array_length(_results,1)]).check_point.status = 'Failed' THEN RETURN; END IF;
 
   END;
 
@@ -95,7 +103,7 @@ manca una combinazione sul check parents_meetings_uq
 
     RETURN;
 
-   EXCEPTION WHEN SQLSTATE '23505' THEN
+   /*EXCEPTION WHEN SQLSTATE '23505' THEN
 
       GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
 
@@ -117,7 +125,11 @@ manca una combinazione sul check parents_meetings_uq
 
 	_results =  _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
 
-        RETURN;
+        RETURN;*/
+     EXCEPTION WHEN OTHERS THEN
+     GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+     _results = _results || assert.sqlstate_constraint_equals(me.full_function_name, me.test_name, me.error, '23505','parents_meetings_uq');
+     IF (_results[array_length(_results,1)]).check_point.status = 'Failed' THEN RETURN; END IF;
 
   END;
   RETURN;
@@ -126,4 +138,4 @@ $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
 ALTER FUNCTION unit_tests_public.parents_meetings_check(boolean)
-  OWNER TO postgres;
+  OWNER TO scuola247_supervisor;
--- schemas/unit_tests_public/functions/parents_meetings_trigger(boolean)/create.sql
+++ schemas/unit_tests_public/functions/parents_meetings_trigger(boolean)/create.sql
@@ -28,12 +28,16 @@ BEGIN
     UPDATE parents_meetings SET teacher = '29122000000000' WHERE  parents_meeting = '33433000000000';
     _results = _results || assert.fail(full_function_name, test_name,'Update was OK but the teacher is not from the same school', NULL::diagnostic.error);
     RETURN;
-    EXCEPTION WHEN SQLSTATE 'U0511' THEN
+    /*EXCEPTION WHEN SQLSTATE 'U0511' THEN
       _results = _results || assert.pass(full_function_name, test_name);
       WHEN OTHERS THEN
         GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
         _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
-        RETURN;
+        RETURN;*/
+    EXCEPTION WHEN OTHERS THEN
+     GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+     _results = _results || assert.sqlstate_equals(me.full_function_name, me.test_name, me.error, 'U0511');
+     IF (_results[array_length(_results,1)]).check_point.status = 'Failed' THEN RETURN; END IF;
   END;
   -------------------------------------------------------------------------
   test_name = 'INSERT parents_meetings with teacher from different school';
@@ -42,12 +46,16 @@ BEGIN
     INSERT INTO public.parents_meetings(parents_meeting,teacher,person,on_date) VALUES ('10033433000000000','29122000000000','6603000000000','2013-10-01 00:00:00');
     _results = _results || assert.fail(full_function_name, test_name,'Insert was OK but the teacher is not from the same school', NULL::diagnostic.error);
     RETURN;
-    EXCEPTION WHEN SQLSTATE 'U0512' THEN
+    /*EXCEPTION WHEN SQLSTATE 'U0512' THEN
       _results = _results || assert.pass(full_function_name, test_name);
       WHEN OTHERS THEN
         GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
         _results = _results || assert.fail(full_function_name, test_name, 'Unexpected exception', error);
-        RETURN;
+        RETURN;*/
+     EXCEPTION WHEN OTHERS THEN
+     GET STACKED DIAGNOSTICS error.returned_sqlstate = RETURNED_SQLSTATE, error.message_text = MESSAGE_TEXT, error.schema_name = SCHEMA_NAME, error.table_name = TABLE_NAME, error.column_name = COLUMN_NAME, error.constraint_name = CONSTRAINT_NAME, error.pg_exception_context = PG_EXCEPTION_CONTEXT, error.pg_exception_detail = PG_EXCEPTION_DETAIL, error.pg_exception_hint = PG_EXCEPTION_HINT, error.pg_datatype_name = PG_DATATYPE_NAME;
+     _results = _results || assert.sqlstate_equals(me.full_function_name, me.test_name, me.error, 'U0512');
+     IF (_results[array_length(_results,1)]).check_point.status = 'Failed' THEN RETURN; END IF;
   END;
   RETURN;
 END
@@ -55,4 +63,4 @@ $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
 ALTER FUNCTION unit_tests_public.parents_meetings_trigger(boolean)
-  OWNER TO postgres;
+  OWNER TO scuola247_supervisor;
