c5a2037dd07b543e271d98597ce6eaded30c507e
Andrea Adami <fol@fulcro.net>
2018-06-20 10:30:43 +0200

    Initial add

---
+++ schemas/diagnostic/types/verbosities/create.sql
@@ -0,0 +1,18 @@
+﻿-- Type: diagnostic.verbosities
+
+-- DROP TYPE diagnostic.verbosities;
+
+CREATE TYPE diagnostic.verbosities AS ENUM
+   ('log',
+    'notice',
+    'warning',
+    'error',
+    'fatal',
+    'panic',
+    'debug1',
+    'debug2',
+    'debug3',
+    'debug4',
+    'debug5');
+ALTER TYPE diagnostic.verbosities
+  OWNER TO scuola247_supervisor;
