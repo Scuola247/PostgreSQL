37307fc5eef4d76456022110dec2c76b0481c0be
Federico Tomelleri <federico.tomelleri@gmail.com>
2018-06-22 19:37:09 +0200

    add constraint

--- schemas/public/tables/persons_addresses/constraints/persons_addresses_ck_city/create.sql
+++ schemas/public/tables/persons_addresses/constraints/persons_addresses_ck_city/create.sql
@@ -1,2 +1,6 @@
+-- Check: public.persons_addresses_ck_city
+
+-- ALTER TABLE public.persons_addresses DROP CONSTRAINT persons_addresses_ck_city;
+
 ALTER TABLE public.persons_addresses
-  ADD CONSTRAINT persons_addresses_ck_city CHECK (length(btrim(city::text)) > 0);
+  ADD CONSTRAINT persons_addresses_ck_city CHECK (length(btrim(city_fiscal_code::text)) > 0);
