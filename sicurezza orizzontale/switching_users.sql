780a1e5fb3a672e7dc3a5a32d8245f886e581dcc
Andrea Adami <fol@fulcro.net>
2018-06-26 19:41:46 +0200

    Auto stash before merge of "master" and "origin/master"
    Structure reorganize

--- sicurezza orizzontale/switching_users.sql
+++
@@ -1,8 +0,0 @@
-﻿SET ROLE 'manager-a@scuola-1.it'
-SET ROLE 'manager-c@scuola-2.it'
-SET ROLE 'manager-e@scuola-28961.it'
-SET ROLE 'student-a@scuola-1.it'
-SET ROLE 'fol@scuola247.it'
-SET ROLE 'matteonovelli.my@gmail.com'
-RESET ROLE
-SELECT school, usename from persons WHERE usename IS not null
