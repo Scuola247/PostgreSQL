2de4360b7ceb00a631dd838fe0386a6fda50367b
Andrea Adami <fol@fulcro.net>
2018-07-17 20:27:53 +0200

    fix some checks

--- schemas/public/trigger functions/tr_grading_meetings_valutations_iu()/create.sql
+++ schemas/public/trigger functions/tr_grading_meetings_valutations_iu()/create.sql
@@ -110,7 +110,7 @@ BEGIN
      JOIN school_years sy ON sy.school_year = gm.school_year
      JOIN schools s ON s.school = sy.school
      JOIN metrics m ON m.school = s.school  
-     JOIN grades g ON g.grade = m.grade
+     JOIN grades g ON g.metric = m.metric
     WHERE gm.grading_meeting = new.grading_meeting
       AND g.grade = new.grade;
       
